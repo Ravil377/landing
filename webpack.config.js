ff83987e0462667ca10e0409e5b6cc750d68316d
Ravil <ravil377@mail.ru>
2021-07-14 02:46:35 +0500

    update slider, main

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,71 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плаг 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // подключите к проекту mini-css-extract-plugin
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-                publicPath: ''
-    },
-    mode: 'development', // добавили режим разработчика
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    
-        open: true // сайт будет открываться сам при запуске npm run dev
+  entry: { main: "./src/pages/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        "test": /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        "use": "babel-loader",
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        "exclude": "/node_modules/",
       },
-    module: {
-        rules: [
-        {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: '/node_modules/'
-        },
-        {
-            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-            type: 'asset/resource'
-        },
-        {
-            test: /\.npm install sass-loader sass webpack --save-devcss$/,
-            use: [MiniCssExtractPlugin.loader, {
-                loader: 'css-loader',
-                options: { importLoaders: 1 }
-            },
-            'postcss-loader']
-        },
-        {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader",
-            ],
-        }
-    ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/pages/index.html' // путь к файлу index.html
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin() // подключение плагина для объединения файлов
-    ]
-}; 
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|ico|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.html", // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
+  ],
+};
